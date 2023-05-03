df9f0a5205968f800ed28d1651817a2bcb0b426c
Marcel Gažík <GazikMarcel@litv.sssvt.cz>
2023-05-03 11:11:44 +0200

    setting dropdown

--- src/app/pages/main/components/header/header.component.ts
+++ src/app/pages/main/components/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { SessionsService } from '../../../../services/sessions.service'
 
 @Component({
   selector: 'app-header',
@@ -7,4 +8,9 @@ import { Component } from '@angular/core';
 })
 export class HeaderComponent {
 
+  public constructor (private session: SessionsService){}
+
+  public onLogout(): void {
+    this.session.logout();
+  }
 }
