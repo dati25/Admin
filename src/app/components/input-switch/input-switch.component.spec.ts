7df3bed7aeeec883eacafadd783c229e6db8a644
dati25 <shartavadavit0@gmail.com>
2023-05-29 19:33:59 +0200

    minor fixes

--- src/app/components/input-switch/input-switch.component.spec.ts
+++ src/app/components/input-switch/input-switch.component.spec.ts
@@ -1,23 +1,22 @@
-// import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-// import { InputSwitchComponent } from './input-switch.component';
+import { InputSwitchComponent } from './input-switch.component';
 
-// describe('InputSwitchComponent', () => {
-//   let component: InputSwitchComponent;
-//   let fixture: ComponentFixture<InputSwitchComponent>;
+describe('InputSwitchComponent', () => {
+  let component: InputSwitchComponent;
+  let fixture: ComponentFixture<InputSwitchComponent>;
 
-//   beforeEach(async () => {
-//     await TestBed.configureTestingModule({
-//       declarations: [ InputSwitchComponent ]
-//     })
-//     .compileComponents();
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [InputSwitchComponent],
+    }).compileComponents();
 
-//     fixture = TestBed.createComponent(InputSwitchComponent);
-//     component = fixture.componentInstance;
-//     fixture.detectChanges();
-//   });
+    fixture = TestBed.createComponent(InputSwitchComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
 
-//   it('should create', () => {
-//     expect(component).toBeTruthy();
-//   });
-// });
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
